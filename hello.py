d793d9a391628e533bf8a031c5ff722c59cb19cb
EllianSLC <ellianlgsamuel@gmail.com>
2025-02-21 11:39:50 -0300

    atividade-python

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Missão 1
+nota = float(input("Digite a nota do aluno: "))
+
+if nota >= 6:
+    print("Aluno aprovado!")
+else:
+    print("Aluno reprovado!")
+
+    # Missão 2
+idade = int(input("Digite sua idade: "))
+
+if idade >= 16:
+    print("Você pode votar.")
+else:
+    print("Você não pode votar.")
